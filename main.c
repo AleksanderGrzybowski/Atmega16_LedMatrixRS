4c7500cf318d12bcfe0df1ceb0a90cdccf7deb65
Aleksander Grzybowski <kelog@orange.pl>
2015-07-26 09:00:45 +0200

    Working multiplexing

--- main.c
+++ main.c
@@ -6,12 +6,62 @@
 #include <avr/sleep.h>
 #include <stdbool.h>
 
+
+
+volatile char display[8];
+
+volatile int cur_row = 0; // current displayed digit
+
+
+ISR(TIMER0_OVF_vect) {
+
+	/* regulate speed of multiplexing, less = slower
+	 * this is way simpler than fiddling with prescalers :)
+	 */
+	TCNT0 = 220;
+
+	cur_row++;
+	if (cur_row == 8) {
+		cur_row = 0;
+	}
+
+	/* turn off all to avoid ghosting */
+	PORTA = 0xff;
+	PORTC = ~(1 << cur_row);
+
+	/* and on */
+	PORTA = ~display[cur_row];
+}
+
+
 int main(void) {
 
 	DDRA = 0xff;
+	DDRC = 0xff;
+
+	/* disable JTAG so we can use all pins */
+	MCUCSR |= (1 << JTD);
+	MCUCSR |= (1 << JTD);
+
+	/* enable timer overflow interrupts for PWM */
+	TIMSK |= (1 << TOIE0);
+	/* set prescaler, selected by experimenting, but works perfectly */
+	TCCR0 |= (1 << CS02);
+	TCCR0 &= ~(1 << CS01);
+	TCCR0 &= ~(1 << CS00);
+	/* this is needed! */
+	sei();
+
 
 	while(1) {
-		_delay_ms(50);
-		PORTA ^= 0xff;
+		int i, j;
+
+		for (i = 0; i < 8; ++i) {
+			for (j = 0; j < 256; j++) {
+				display[i] = j;
+				_delay_ms(10);
+			}
+		}
+
 	}
 }
