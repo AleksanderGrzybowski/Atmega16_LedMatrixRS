b6514c2f36540487b0eb2557c295e11846c7d03c
Aleksander Grzybowski <kelog@orange.pl>
2015-07-26 13:04:36 +0200

    Random patterns

--- client/test.rb
+++ client/test.rb
@@ -4,4 +4,9 @@ require 'serialport'
 
 ser = SerialPort.new("/dev/ttyUSB0", 9600, 8, 1, SerialPort::NONE)
 
-ser.write "ab12345678"
+1.upto(100) do
+	tosend = ("ab".encode! "ASCII")
+	tosend += ( (1..10).map {|t| rand(0..255).chr}).join
+	ser.write tosend
+	sleep 1
+end
